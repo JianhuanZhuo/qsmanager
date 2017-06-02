0e0cbfa639a639c1f1615487c9a5aa4471447e62
unknown <>
2017-06-03 02:49:10 +0800

    first commit

---
+++ src/main/java/cn/keepfight/qsmanager/AppLoader.java
@@ -0,0 +1,12 @@
+package cn.keepfight.qsmanager;
+
+/**
+ * loader of client application.
+ * Created by tom on 2017/5/30.
+ */
+public class AppLoader {
+
+    public static void main(String[] args) {
+
+    }
+}
